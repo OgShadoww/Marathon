ffe515f1aab9f3911ee5e68002b0a08cf7a69938
Orest <galenzao@gmail.com>
2022-12-25 18:07:05 +0200

    change light

--- src/section/Content.jsx
+++ src/section/Content.jsx
@@ -5,7 +5,7 @@ const Content = () => {
     return (
         <div className='flex flex-col justify-center items-center'>
             <h1 className='uppercase text-[#DAD0FF] text-[44px] mb-[100px]'>контент марафону</h1>
-            <img className='w-[7%]' src={Arrow}/>
+            <img className='w-[7%] mb-[20px]' src={Arrow}/>
             <iframe width="966" height="543" src="https://www.youtube.com/embed/VpRNtyuGwnI" title="CS |  Вступ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     );
