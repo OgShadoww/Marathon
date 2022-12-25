d6878f4334ae76d0746a094834ca8ff591519ed6
Orest <galenzao@gmail.com>
2022-12-25 19:16:59 +0200

    adaptive

--- src/section/Access.jsx
+++ src/section/Access.jsx
@@ -7,19 +7,19 @@ import React from 'react';
 
 const Access = () => {
     return (
-        <div className='relative access w-full h-[100vh] mb-[100px]'>
+        <div className='relative access w-full h-[100vh] mb-0 lg:mb-[100px] md:mb-[100px]'>
             <img src={AtmosphereLeft} className='absolute w-[30%] bottom-0 left-[-2%]' alt="" />
             <img src={Background} className='absolute top-[40%] opacity-[0.4]' alt="" />
             <div className='access-light-center'></div>
             <div className='access-light-left'></div>
-            <div className='flex justify-around items-baseline px-[30px]'>
-                <div className='flex flex-col justify-start items-start gap-20px px-[50px]'>
+            <div className='flex lg:justify-around md:lg:justify-around justify-center sm:justify-center lg:items-baseline md:items-baseline sm:items-center items-center px-[30px]'>
+                <div className='flex flex-col lg:justify-start md:justify-start items-center lg:items-start md:items-start sm:items-center gap-20px px-[50px]'>
                     <h1 className='text-[#DAD0FF] font-bold text-[64px]'>ДОСТУП</h1>
-                    <p className='text-[#BAB3D2] w-[80%] lg:w-[40%] md:w-[60%] sm:w-[70%] text-left'>
+                    <p className='text-[#BAB3D2] w-[100%] lg:w-[40%] md:w-[60%] sm:w-[100%] text-center lg:text-left md:text-left'>
                         На даній сторінці ви можете переглядати всю інформацію яку ми проходимо у вигляді відео-уроків, стрімів та методичок. Доступ до даних уроків завжди з вами до закінчення марафону
                     </p>
                 </div>
-                <div className='relative w-[50%] flex items-center justify-center'>
+                <div className='relative w-[50%] lg:flex items-center justify-center hidden md:block sm:hidden'>
                     <img src={ImgBottom} className='w-[100%] opacity-[0.8]' alt="" />
                     <div className='universe-light ml-[50%] translate-x-[-50%]]'>
 
--- src/section/Begin.jsx
+++ src/section/Begin.jsx
@@ -9,10 +9,10 @@ const Begin = () => {
         <div className='relative flex flex-col lg:flex-row md:flex-row sm:flex-row justify-center items-center gap-[50px] begin h-[80vh] w-full px-[30px] '>
             <div className='header-light'></div>
             <img src={TopSchedule} className='absolute top-0 right-0 w-[60%] z-0' alt="" />
-            <img src={Img} className='w-[30%] lg:w-[30%] md:w-[40%] sm:w-[40%] z-10' alt="" />
+            <img src={Img} className=' hidden lg:block md:block sm:hidden lg:w-[30%] md:w-[40%] sm:w-[40%] z-10' alt="" />
             <div className='flex flex-col justify-center items-center lg:items-start md:items-start sm:items-center lg:justify-start md:justify-start'>
                 <img src={Text} className='w-[70%] mb-[20px]' alt="" />
-                <p className='text-center lg:text-left md:text-left sm:text-[center] max-w-[700px] mb-[30px]'>
+                <p className='text-center lg:text-left md:text-left sm:text-[center] lg:max-w-[700px] md:max-w-[700px] sm:max-w-[600px] max-w-[400px] mb-[30px]'>
                     Cosmos - криптовалютний марафон націлений на загальний розвиток українського ком’юніті в сфері криптовалют. В даному марафоні ми будемо вивчати основи торгівлі на ф’ючерсах. В основі аналізу ринку в нас лежить концепція Smart Money
                 </p>
                 <button className='uppercase z-10 text-[#B7DDFF] bg-gradient-to-b from-[#3387D0] to-[#62A4DE] w-[70%] px-[20px] py-[20px] rounded-[10px]'>
@@ -22,7 +22,7 @@ const Begin = () => {
             <div className='begin-light'>
 
             </div>
-            <img src={BottomSchedule} className='absolute bottom-[-40%] left-0 w-[50%]' alt="" />
+            <img src={BottomSchedule} className='absolute bottom-[-80%] left-0 w-[50%]' alt="" />
         </div>
     );
 };
