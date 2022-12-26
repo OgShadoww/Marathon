90e0c09919e8fe1b040ebdb65474d4f2a69f772a
Orest <galenzao@gmail.com>
2022-12-26 08:33:21 +0200

    Change App

--- src/section/Begin.jsx
+++ src/section/Begin.jsx
@@ -6,10 +6,10 @@ import Img from '../img/begin/begin.png'
 
 const Begin = () => {
     return (
-        <div className='relative flex flex-col lg:flex-row md:flex-row sm:flex-row justify-center items-center gap-[50px] begin h-[80vh] w-full px-[30px] '>
+        <div className='relative flex flex-col lg:flex-row md:flex-row sm:flex-row justify-center items-center gap-[50px] begin h-[80vh] w-full px-[50px] '>
             <div className='header-light'></div>
             <img src={TopSchedule} className='absolute top-0 right-0 w-[60%] z-0' alt="" />
-            <img src={Img} className=' hidden lg:block md:block sm:hidden lg:w-[30%] md:w-[40%] sm:w-[40%] z-10' alt="" />
+            <img src={Img} className='mr-[50px] hidden lg:block md:block sm:hidden lg:w-[30%] md:w-[40%] sm:w-[40%] z-10' alt="" />
             <div className='flex flex-col justify-center items-center lg:items-start md:items-start sm:items-center lg:justify-start md:justify-start'>
                 <img src={Text} className='w-[70%] mb-[20px]' alt="" />
                 <p className='text-center lg:text-left md:text-left sm:text-[center] lg:max-w-[700px] md:max-w-[700px] sm:max-w-[600px] max-w-[400px] mb-[30px]'>
