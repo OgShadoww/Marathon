fd21acbe01163c55407869fe3a5cd218e24149e1
Orest <galenzao@gmail.com>
2022-12-26 13:21:04 +0200

    change text

--- src/section/Content.jsx
+++ src/section/Content.jsx
@@ -34,7 +34,7 @@ const Content = () => {
                     <LineSuccess num={9} current={current} text="Робота з TradingView" changeCurrent={changeCurrent}/>
                     <LineSuccess num={10} current={current} text="Комплексна робота " changeCurrent={changeCurrent}/>
                 </div>
-                <div className='bg-[#D9D9D9] flex flex-col gap-[20px] items-end bg-opacity-[0.1] w-[80%] px-[15px] py-[10px]'>
+                <div className='bg-[#D9D9D9] flex flex-col gap-[20px] items-end bg-opacity-[0.1] w-[70%] px-[15px] py-[10px] youtube'>
                     <iframe width="966" height="543" src="https://www.youtube.com/embed/VpRNtyuGwnI" title="CS |  Вступ" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                     <button onClick={() => nextCurrent()} className='bg-gradient-to-r from-[#3588D2] to-[#91C5F2] px-[50px] py-[15px] text-white rounded-[10px] w-[30%]'>Наступий</button>
                 </div>
