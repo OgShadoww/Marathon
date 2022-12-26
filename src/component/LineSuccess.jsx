be9ff16b5c00107050de6a39ac47abd4ebeb19d8
Orest <galenzao@gmail.com>
2022-12-26 12:05:26 +0200

    add content

--- src/component/LineSuccess.jsx
+++ src/component/LineSuccess.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 
-const LineSuccess = ({num, position}) => {
-    const baseStyle = ['flex flex-col justify-center items-center text-[26px] font-bold rounded-[50%] w-[45px] h-[45px] ml-[-6%]', 'bg-[#D9D0FF] text-[#8880A8]']
-    const thisPosition = num
+const LineSuccess = ({num, current, changeCurrent, text}) => {
+    const baseStyle = ['flex flex-col justify-center items-center text-[26px] font-bold rounded-[50%] w-[45px] h-[45px] ml-[-9%] z-10 cursor-pointer transition-[0.4s]', 'bg-[#D9D0FF] text-[#8880A8]']
+    const titleStyle = ['transition-[0.4s]', 'text-[#676081]']
+    const numberLesson = ['font-bold transition-[0.4s]', 'text-[#676081]']
 
-    if(position === num) {
+    if(current === num) {
         baseStyle.pop()
         baseStyle.push('bg-[#00F17D] border-[#fff] border-[2px] text-[#fff] shadow-lg')
+        titleStyle.pop()
+        titleStyle.push('text-[#00F17D]')
+        numberLesson.pop()
+        numberLesson.push('text-[#FFFFFF]')
     }
 
     return (
         <div className='flex items-center gap-[10px]'>
-            <div className={baseStyle.join(' ')}>
+            <div onClick={() => changeCurrent(num)} className={baseStyle.join(' ')}>
                 {num}
             </div>
             <div className='flex flex-col items-start '>
-                <div className='font-bold'>
+                <div className={numberLesson.join(' ')}>
                     Урок {num}
                 </div>
-                <div className=''>
+                <div className={titleStyle.join(' ')}>
                     Вступний урок   
                 </div>
             </div>
--- src/section/Content.jsx
+++ src/section/Content.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LineSuccess from '../component/LineSuccess';
 import Arrow from '../img/arrow.png'
 
 const Content = () => {
+    const [current, setCurrent] = useState(1)
+
+    const changeCurrent = (i) => {
+        setCurrent(i)
+        console.log('f')
+    }
+    
     return (
         <div className='flex flex-col justify-center items-center'>
             <h1 className='uppercase text-[#DAD0FF] text-[44px] mb-[100px]'>контент марафону</h1>
-            <img className='w-[7%] mb-[20px]' src={Arrow}/>
-            <div className='w-full youtube px-[20px]'>
-                {/* <iframe width="966" height="543" src="https://www.youtube.com/embed/VpRNtyuGwnI" title="CS |  Вступ" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
-            </div>
-            <div className='line-success '>
-                <LineSuccess num={1} position={1}/>
-                <LineSuccess num={2}/>
-                <LineSuccess num={3}/>
-                <LineSuccess num={4}/>
-                <LineSuccess num={5}/>
-                <LineSuccess num={6}/>
-                <LineSuccess num={7}/>
-                <LineSuccess num={8}/>
-                <LineSuccess num={9}/>
+            <img className='w-[7%] mb-[50px]' src={Arrow}/>
+            <div className='flex justify-between gap-[50px] w-full px-[100px]'>
+                <div className='line-success'>
+                    <LineSuccess num={1} current={current} changeCurrent={changeCurrent}/>
+                    <LineSuccess num={2} current={current} changeCurrent={changeCurrent}/>
+                    <LineSuccess num={3} current={current} changeCurrent={changeCurrent}/>
+                    <LineSuccess num={4} current={current} changeCurrent={changeCurrent}/>
+                    <LineSuccess num={5} current={current} changeCurrent={changeCurrent}/>
+                    <LineSuccess num={6} current={current} changeCurrent={changeCurrent}/>
+                    <LineSuccess num={7} current={current} changeCurrent={changeCurrent}/>
+                    <LineSuccess num={8} current={current} changeCurrent={changeCurrent}/>
+                    <LineSuccess num={9} current={current} changeCurrent={changeCurrent}/>
+                </div>
+                <div className='bg-[#D9D9D9] flex flex-col gap-[20px] items-end bg-opacity-[0.1] w-[80%] px-[15px] py-[10px]'>
+                    <iframe width="966" height="543" src="https://www.youtube.com/embed/VpRNtyuGwnI" title="CS |  Вступ" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <button onClick={() => setCurrent(current+1)} className='bg-gradient-to-r from-[#3588D2] to-[#91C5F2] px-[50px] py-[15px] text-white rounded-[10px] w-[30%]'>Наступий</button>
+                </div>
             </div>
         </div>
     );
