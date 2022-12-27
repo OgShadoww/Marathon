bf7d39e3e51580e6fe3305bf352bf02edfe59cd7
Orest <galenzao@gmail.com>
2022-12-27 09:55:57 +0200

    change light

--- src/section/Begin.jsx
+++ src/section/Begin.jsx
@@ -3,12 +3,14 @@ import Text from '../img/begin/COSMOS.png'
 import TopSchedule from '../img/begin/top-schedule.png'
 import BottomSchedule from '../img/begin/bottom-schedule.png'
 import Img from '../img/begin/begin.png'
+import Light from '../img/light.png'
 
 const Begin = ({buttonHandler, Elementref}) => {
 
     return (
         <div className='begin relative flex flex-col lg:flex-row md:flex-row sm:flex-row justify-center items-center gap-[50px] begin h-[80vh] w-full px-[50px] '>
-            {/* <div className='header-light hidden lg:block sm:hidden'></div> */}
+            <img src={Light} className='absolute top-[-40%] left-[-40%] w-[80%] hidden lg:block md:hidden sm:hidden' alt="" />
+            <img src={Light} className='absolute top-[40%] right-[-40%] w-[80%] hidden lg:block md:hidden sm:hidden' alt="" />
             <img src={TopSchedule} className='absolute pointer-events-none top-0 right-0 w-[60%] z-0' alt="" />
             <img src={Img} className='mr-[50px] hidden lg:block md:block sm:hidden lg:w-[30%] md:w-[40%] sm:w-[40%] z-10' alt="" />
             <div className='flex flex-col justify-center items-center lg:items-start md:items-start sm:items-center lg:justify-start md:justify-start'>
@@ -20,7 +22,7 @@ const Begin = ({buttonHandler, Elementref}) => {
                     НАВЧАТИСЬ БЕЗКОШТОВНО
                 </button>
             </div>
-            {/* <div className='begin-light hidden lg:block sm:hidden'></div> */}
+            {/* <img src={Light} className='absolute right-0 top-[50%] w-[40%]' alt="" /> */}
             <img src={BottomSchedule} className='absolute pointer-events-none bottom-[-80%] left-0 w-[50%]' alt="" />
         </div>
     );
