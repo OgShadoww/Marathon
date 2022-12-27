99b0352851e6225fac50cb358ff58db00fdbc44d
Orest <galenzao@gmail.com>
2022-12-27 07:07:43 +0200

    z index change

--- src/component/VideoItem.jsx
+++ src/component/VideoItem.jsx
@@ -8,8 +8,8 @@ const VideoItem = ({nextCurrent, prevCurrent, video, title, question}) => {
             </h1>
             {video}
             <div className='flex justify-between items-center w-full mb-[20px]'>
-                <button onClick={() => prevCurrent()} className='bg-gradient-to-r from-[#3588D2] to-[#91C5F2] px-[50px] py-[15px] text-white rounded-[10px] w-[30%] text-[16px] lg:text-[24px] md:text-[22px] sm:text-[20px] flex items-center justify-center'>Попередній</button>
-                <button onClick={() => nextCurrent()} className='bg-gradient-to-r from-[#3588D2] to-[#91C5F2] px-[50px] py-[15px] text-white rounded-[10px] w-[30%] text-[16px] lg:text-[24px] md:text-[22px] sm:text-[20px] flex items-center justify-center'>Наступий</button>
+                <button onClick={() => prevCurrent()} className='bg-gradient-to-r z-10 from-[#3588D2] to-[#91C5F2] px-[50px] py-[15px] text-white rounded-[10px] w-[30%] text-[16px] lg:text-[24px] md:text-[22px] sm:text-[20px] flex items-center justify-center'>Попередній</button>
+                <button onClick={() => nextCurrent()} className='bg-gradient-to-r z-10 from-[#3588D2] to-[#91C5F2] px-[50px] py-[15px] text-white rounded-[10px] w-[30%] text-[16px] lg:text-[24px] md:text-[22px] sm:text-[20px] flex items-center justify-center'>Наступий</button>
             </div>
             <div className='text-left'>
                 <h3 className='text-[22px] text-[#bdbac5] font-bold mb-[10px]'>Завдання</h3>
